11a18abca537b70e9ef62e46169f20a231628c91
ozkavosh <augustoslva@hotmail.com>
2022-10-19 10:36:38 -0300

    Ejercicio clase 19/10

--- App.tsx
+++ App.tsx
@@ -3,9 +3,11 @@ import {
   StyleSheet,
   View,
   Text,
-  SafeAreaView,
   ScrollView,
   Image,
+  ActivityIndicator,
+  Button,
+  Switch,
   TextInput,
 } from 'react-native';
 import pokemonList from './src/db/pokemonList';
@@ -20,12 +22,20 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     height: 75,
   },
+  search:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 45,
+  },
   input: {
     height: 40,
-    width: '100%',
+    flexGrow: 2,
     margin: 12,
     borderWidth: 1,
     padding: 10,
+  },
+  button: {
+    flexGrow: 1,
   },
   pokemon: {
     width: '100%',
@@ -57,42 +67,77 @@ const capitalize = (string: string): string => {
 };
 
 const App = () => {
+  const [disabled, setDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState('');
   const [pokemons, setPokemons] = useState([]);
 
-  useEffect(() => {
+  const searchPokemons = () => {
     if (search) {
-      setPokemons(pokemonList.filter(pokemon => pokemon.name.includes(search.toLowerCase())));
+      setPokemons(
+        pokemonList.filter(pokemon =>
+          pokemon.name.includes(search.toLowerCase()),
+        ),
+      );
     } else {
       setPokemons(pokemonList);
     }
-  }, [search]);
+    setLoading(false);
+  }
+
+  const handleSearch = () => {
+    setLoading(true);
+    setTimeout(searchPokemons, 3000);
+  }
+
+  useEffect(() => {
+    setPokemons(pokemonList);
+  }, [])
 
   return (
-    <ScrollView
-      style={styles.mainContainer}
-      contentContainerStyle={{alignItems: 'center'}}>
+    <View style={[styles.mainContainer, {alignItems: 'center'}]}>
       <Image source={require('./src/img/logo.png')} />
-      <TextInput
-        onChangeText={newText => setSearch(newText)}
-        value={search}
-        style={styles.input}
-        placeholder="Ingresa el nombre del pokemon"
-      />
+      <View style={{ alignItems: 'center', flexDirection: 'row', marginVertical: 15}}>
+        <Text style={{marginRight: 5}}>Desactivar Búsqueda</Text>
+        <Switch onValueChange={_ => setDisabled((prev: boolean) => !prev)} value={disabled}></Switch>
+      </View>
+      <View style={styles.search}>
+        <TextInput
+          onChangeText={(newText: string) => setSearch(newText)}
+          onSubmitEditing={handleSearch}
+          value={search}
+          editable={!disabled}
+          style={[styles.input, (disabled || loading) && { backgroundColor: 'gray' }]}
+          placeholder="Ingresa el nombre del pokemon"
+        />
+        {loading ? <ActivityIndicator/> : <Button disabled={disabled} style={styles.button} title="Buscar" onPress={handleSearch} />}
+      </View>
+
       {pokemons.length ? (
-        pokemons.map((pokemon, index) => (
-          <View key={index} style={styles.pokemon}>
-            <Image style={styles.pokemonImg} source={{uri: pokemon.url}} />
-            <Text style={styles.pokemonText}>{capitalize(pokemon.name)}</Text>
-          </View>
-        ))
+        <ScrollView style={{width: '100%'}}>
+          {pokemons.map(
+            (pokemon: {url: string; name: string}, index: number) => (
+              <View key={index} style={styles.pokemon}>
+                <Image style={styles.pokemonImg} source={{uri: pokemon.url}} />
+                <Text style={styles.pokemonText}>
+                  {capitalize(pokemon.name)}
+                </Text>
+              </View>
+            ),
+          )}
+        </ScrollView>
       ) : (
         <View>
           <Text>No se pudo encontrar ningún Pokemon con el nombre</Text>
-          <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 20 }}>{`"${search}"`}</Text>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontWeight: 'bold',
+              fontSize: 20,
+            }}>{`"${search}"`}</Text>
         </View>
       )}
-    </ScrollView>
+    </View>
   );
 };
 
